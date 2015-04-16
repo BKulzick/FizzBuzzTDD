7c39d648af653ec10eeaa9b56e6ae412b72ce9fc
BKulzick <BryanKulczycki@gmail.com>
2015-04-16 11:18:01 -0400

    Added Before to intialize fizzBuzz object, passed Multiples of 3

--- FizzBuzzTDD/src/main/FizzBuzz.java
+++ FizzBuzzTDD/src/main/FizzBuzz.java
@@ -3,7 +3,10 @@ package main;
 public class FizzBuzz {
 
 	public String fizz(Integer inputNumber) {
-		
+		if (inputNumber % 3 == 0) {
+			return "Fizz";
+		}
+
 		return inputNumber.toString();
 	}
 
--- FizzBuzzTDD/src/test/FizzBuzzTest.java
+++ FizzBuzzTDD/src/test/FizzBuzzTest.java
@@ -3,18 +3,41 @@ package test;
 import static org.junit.Assert.*;
 import main.FizzBuzz;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
 
+	FizzBuzz fizzBuzz;
+
+	@Before
+	public void setUp() {
+		fizzBuzz = new FizzBuzz();
+	}
+
 	@Test
 	public void whenFizzBuzzIsPassedANumberItReturnsThatNumber() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("1", fizzBuzz.fizz(1));
 	}
+
 	@Test
-	public void whenFizzBuzzIsPassedATwoItReturnsaTwo(){
-		FizzBuzz fizzBuzz = new FizzBuzz();
+	public void whenFizzBuzzIsPassedATwoItReturnsaTwo() {
 		assertEquals("2", fizzBuzz.fizz(2));
 	}
+
+	@Test
+	public void whenFizzBuzzIsPassedAThreeSayFizz() {
+		assertEquals("Fizz", fizzBuzz.fizz(3));
+
+	}
+
+	@Test
+	public void whenFizzBuzzIsPassedAnyMultipleOfThreeSayFizz() {
+		assertEquals("Fizz", fizzBuzz.fizz(6));
+		assertEquals("Fizz", fizzBuzz.fizz(9));
+
+	}
+
+
+
 }
